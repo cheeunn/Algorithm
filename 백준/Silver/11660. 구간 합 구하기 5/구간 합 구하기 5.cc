f852b556bce720a4b7266ba8f70b22c3cdf578e5
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-01-01 15:50:17 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 120 ms, Memory: 10356 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	int x1, y1, x2, y2;
+	cin >> n >> m;
+	int result;
+	vector<vector <int>> A(n + 1, vector<int>(n + 1, 0));
+	vector<vector <int>> D(n + 1, vector<int>(n + 1, 0));
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> A[i][j];
+			D[i][j] = D[i - 1][j] + D[i][j - 1] - D[i - 1][j - 1] + A[i][j];
+		}
+	}
+
+	for (int i = 0; i < m; i++) {
+		cin >> x1 >> y1 >> x2 >> y2;
+		result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+		cout << result << "\n";
+	}
+
+}
