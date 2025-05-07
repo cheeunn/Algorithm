43e40e3bf89c8416157e05619ca2e56801a73ada
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-05-08 00:01:26 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -2,23 +2,13 @@
 #include <vector>
 
 using namespace std;
-void dfs(int h, int w, vector<int>& answer, int br, int yw) {
-    if(h * w > br + yw) return;
-    if(h * w == br + yw) {
-        answer.push_back(w);
-        answer.push_back(h);
-        return;
-    }
-    dfs(h, w + 2, answer, br, yw);
-    dfs(h + 2, w, answer, br, yw);
-}
 vector<int> solution(int brown, int yellow) {
-    vector<int> answer;
-    for(int yh = 1; yh * yh <= yellow; yh++) {
-        if(yellow % yh) continue;
-        int yw = yellow / yh;
-        dfs(yh + 2, yw + 2, answer, brown, yellow);
-        if(!answer.empty()) break;
+    int wh = brown / 2 + 2;
+    int h = 3;
+    int w = wh - h;
+    while(w >= h){
+        if(w * h == brown + yellow) break;
+        w--; h++;
     }
-    return answer;
+    return vector<int>{w, h};
 }
