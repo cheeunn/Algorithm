2cfe62d4119a3e04f5fd380d68a1ca56d68a3116
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-05-08 17:07:23 +0900

    [level 2] Title: 타겟 넘버, Time: 4.50 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+int n;
+int answer;
+void dfs(int idx, int sum, vector<int>& numbers, int target) {
+    if(idx == n) {
+        if(sum == target) answer++;
+        return;
+    }
+    dfs(idx + 1, sum + numbers[idx], numbers, target);
+    dfs(idx + 1, sum - numbers[idx], numbers, target);
+}
+int solution(vector<int> numbers, int target) {
+    
+    n = numbers.size();
+    dfs(0, 0, numbers, target);
+    return answer;
+}
