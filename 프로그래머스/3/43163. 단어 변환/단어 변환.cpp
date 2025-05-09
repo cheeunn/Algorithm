7d861788ecab46d29509625aadda615374f2a4e3
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-05-09 22:20:01 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,34 +1,41 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <utility>
+#include <queue>
 using namespace std;
-int tries = 100;
+
 int visited[52];
-bool convertable(string from, string to) {
+
+bool possible(string from, string to) {
     int cnt = 0;
     for(int i = 0; i < from.size(); i++) {
         if(from[i] != to[i]) cnt++;
     }
+    
     if(cnt == 1) return true;
-    return false;
-}
-void dfs(string cur, string target, vector<string> words, int count) {
-    if(cur == target) {
-        tries = min(tries, count);
-        return;
-    }
-    for(int i = 0; i < words.size(); i++) {
-        if(visited[i] != 0 || convertable(cur, words[i]) == false) continue;
-        visited[i] = 1;
-        dfs(words[i], target, words, count + 1);
-        visited[i] = 0;
-    }
+    else return false;
 }
 int solution(string begin, string target, vector<string> words) {
     int answer = 0;
-    fill(visited, visited + words.size(), 0);
-    dfs(begin, target, words, 0);
-    answer = tries;
-    if(answer == 100) answer = 0;
+    queue<pair<string, int>> Q;
+    string temp;
+    int num;
+    Q.push({begin, 0});
+    while(!Q.empty()) {
+        temp = Q.front().first;
+        num = Q.front().second;
+        Q.pop();
+        if(temp == target) {
+            answer = num;
+            break;
+        }
+        for(int i = 0; i < words.size(); i++) {
+            if(visited[i]) continue;
+            if(possible(temp, words[i])) {
+                visited[i] = 1;
+                Q.push({words[i], num + 1});
+            }
+        }
+    }
     return answer;
 }
