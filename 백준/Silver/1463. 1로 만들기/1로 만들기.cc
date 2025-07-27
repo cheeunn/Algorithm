306078307070ff2360cbd1dd1b3aefddf5536e7c
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-07-27 16:49:02 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -11,23 +11,9 @@ int main() {
 	cin >> n;
 	d[1] = 0;
 	for (int i = 2; i <= n; i++) {
-		if (i % 2 == 0) { // 2로 나누어 떨어질 때
-			if (i % 3 == 0) {
-				d[i] = min(d[i / 2] + 1, d[i / 3] + 1);
-				d[i] = min(d[i], d[i - 1] + 1);
-			}
-			else {
-				d[i] = min(d[i / 2] + 1, d[i - 1] + 1);
-			}
-		}
-		else {
-			if (i % 3 == 0) {
-				d[i] = min(d[i / 3] + 1, d[i - 1] + 1);
-			}
-			else {
-				d[i] = d[i - 1] + 1;
-			}
-		}
+		d[i] = d[i - 1] + 1;
+		if (i % 2 == 0) d[i] = min(d[i], d[i / 2] + 1);
+		if (i % 3 == 0) d[i] = min(d[i], d[i / 3] + 1);
 	}
 
 	cout << d[n];
