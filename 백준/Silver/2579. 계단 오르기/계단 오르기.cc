6686abf9c0e19ac9e757957547820e01184a3a6e
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-07-27 15:53:40 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -3,7 +3,8 @@ using namespace std;
 
 int k, n;
 int S[302];
-int d[302][3];
+int d[302];
+int sum;
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -11,15 +12,13 @@ int main() {
 	cin >> k;
 	for (int i = 1; i <= k; i++) {
 		cin >> S[i];
+		sum += S[i];
 	}
-	d[1][1] = S[1];
-	d[1][2] = 0;
-	d[2][1] = S[2];
-	d[2][2] = S[1] + S[2];
-
-	for (int i = 3; i <= k; i++) {
-		d[i][1] = max(d[i - 2][1], d[i - 2][2]) + S[i];
-		d[i][2] = d[i-1][1] + S[i];
+	d[1] = S[1];
+	d[2] = S[2];
+	d[3] = S[3];
+	for (int i = 4; i <= k; i++) {
+		d[i] = min(d[i - 2], d[i - 3]) + S[i];
 	}
-	cout << max(d[k][1], d[k][2]);
+	cout << sum - min(d[k-2], d[k-1]);
 }
