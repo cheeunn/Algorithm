0383bc10461a1a137d68fa1a239b5973538f2789
cheeunn <97201032+cheeunn@users.noreply.github.com>
2025-10-15 00:27:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 108 ms, Memory: 6124 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -7,22 +7,27 @@ int board[1025][1025];
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	cout.tie(0);
+
+	int tmp;
 
 	cin >> n >> m;
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-			int tmp;
 			cin >> tmp;
-			board[i][j] = tmp + board[i - 1][j] + board[i][j - 1] - board[i - 1][j - 1];
+			board[i][j] = board[i][j - 1] + board[i - 1][j] - board[i - 1][j - 1] + tmp;
  		}
 	}
 
 	while (m--) {
-		int x1, y1, x2, y2;
+		int ans;
+		int x1, x2, y1, y2;
+
 		cin >> x1 >> y1 >> x2 >> y2;
-		int ans = board[x2][y2] - board[x2][y1 - 1] - board[x1 - 1][y2] + board[x1 - 1][y1 - 1];
+		ans = board[x2][y2] - board[x2][y1 - 1] - board[x1 - 1][y2] + board[x1 - 1][y1 - 1];
 		cout << ans << "\n";
 	}
+
+
+
 }
