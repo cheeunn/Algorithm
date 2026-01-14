a346590fed3918b4f779e45c9d78a5c95451b151
cheeunn <97201032+cheeunn@users.noreply.github.com>
2026-01-14 23:57:21 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -1,23 +1,5 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int a1, a0, c, n;
-	bool ans;
-	
-	cin >> a1 >> a0 >> c >> n;
-	if (a1 > c) {
-		ans = 0;
-	}
-	else if (a1 == c) {
-		if (a0 > 0) ans = 0;
-		else ans = 1;
-	}
-	else {
-		ans = a1 * n + a0 <= c * n ? 1 : 0;
-	}
-	
-	cout << ans;
+#include <stdio.h>
+int a,b,c,n;
+int main(){
+	scanf("%d %d %d %d",&a,&b,&c,&n),printf("%d",a<=c && a*n+b<=c*n);
 }
